8e1ee7066e16a61652551db8576e6cb1ef885b68
Hwnrique <carlosriquerocha57@gmail.com>
2025-12-07 11:29:48 -0300

    update follow

--- src/routes/Follow.tsx
+++ src/routes/Follow.tsx
@@ -1,10 +1,33 @@
 const About = () => {
   return (
     <>
-      <main className="w-2/3 mx-auto p-10 mt-10 text-center mb-96">
-        <h1 className="text-cyan-600 font-bold text-5xl">Achou legal? Junte-se a nós</h1>
-        <p className="text-white text-xl p-4">
-          Faça parte do nosso grupo do Whatsapp <a className="font-bold text-cyan-600" href="https://chat.whatsapp.com/LocwYTvUMX52CP7JXYrY6G?mode=hqrt3">clicando aqui</a>
+      <main
+        className="
+          w-11/12
+          sm:w-4/5
+          md:w-2/3
+          mx-auto
+          p-4
+          sm:p-6
+          md:p-10
+          mt-10
+          text-center
+          mb-40
+        "
+      >
+        <h1 className="text-cyan-600 font-bold text-3xl sm:text-4xl md:text-5xl">
+          Achou legal? Junte-se a nós
+        </h1>
+
+        <p className="text-white text-lg sm:text-xl p-2 sm:p-4">
+          Faça parte do nosso grupo do Whatsapp{" "}
+          <a
+            className="font-bold text-cyan-600 underline-offset-2 hover:underline"
+            href="https://chat.whatsapp.com/LocwYTvUMX52CP7JXYrY6G?mode=hqrt3"
+            target="_blank"
+          >
+            clicando aqui
+          </a>
         </p>
       </main>
     </>
