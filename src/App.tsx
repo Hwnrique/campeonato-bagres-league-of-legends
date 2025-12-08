7c2fb055dda04663afdfe693c5fb1b7dc49ae558
Hwnrique <carlosriquerocha57@gmail.com>
2025-12-08 09:58:05 -0300

    scroll update

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <Navbar />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </>
